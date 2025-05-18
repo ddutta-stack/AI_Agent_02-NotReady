f92f039bac9dc60a6352166a4e18b16684ca2992
ddutta <dibyendu.dutta08@gmail.com>
2025-05-18 22:44:30 +0530

    Updating new packages

--- Agents/New_Agent_001.py
+++ Agents/New_Agent_001.py
@@ -1,3 +1,5 @@
 # New Agents   
 # This is a new agent that will be used for testing purposes.
 # It is a simple agent that will respond to user input with a predefined message.
+from selenium import webdriver
+from selenium.webdriver.common.by import By 
